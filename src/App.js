40add0d444c3669e31cb44fc4775f054b51bed49
Matt Darveniza <matt.darveniza@biarri.com>
2019-11-22 10:44:50 +1100

    removed cruft

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 // eslint-disable-next-line
 import Worker from 'workerize-loader!./test.worker'
-import logo from './logo.svg';
 import './App.css';
 
 const worker = new Worker()
@@ -9,26 +8,12 @@ const worker = new Worker()
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={() => {
-          worker.postMessage({})
-        }}
-        >
-          Worker
-        </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {
+        worker.postMessage({})
+      }}
+      >
+        Worker
+      </button>
     </div>
   );
 }
